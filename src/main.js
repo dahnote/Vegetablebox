401509eae9bd88e222f89d2d300e1330caf222b9
dahnote <dahnote1993@gmail.com>
2021-06-14 09:27:50 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(router);
+app.use(VueAxios, axios);
+app.mount('#app');
